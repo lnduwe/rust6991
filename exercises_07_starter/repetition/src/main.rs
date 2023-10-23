0d2292b4ccdc7a8e4edafbc59ec05aef0d7d8a43
orca <z5465340@ad.unsw.edu.au>
2023-10-24 05:34:06 +1100

    repitition

--- exercises_07_starter/repetition/src/main.rs
+++ exercises_07_starter/repetition/src/main.rs
@@ -3,14 +3,19 @@ fn print_success() {
     println!("Yay, the if statement worked.");
 }
 ////////// DO NOT CHANGE ABOVE HERE /////////
-
 // TODO: create `if_any!()` macro.
-
+macro_rules! if_any {
+    ($($expr:expr),*; {$($block:tt)*}) => {
+        if $($expr)||*  {
+             $($block)*
+        }
+    };
+}
 
 ////////// DO NOT CHANGE BELOW HERE /////////
 
 fn main() {
     if_any!(false, 0 == 1, true; {
-        print_success();
+                print_success();
     })
 }
