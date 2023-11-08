28590b24627f253d6b33712af7c81447cf92d752
orca <z5465340@ad.unsw.edu.au>
2023-11-08 22:18:13 +1100

    pars

--- pars/src/main.rs
+++ pars/src/main.rs
@@ -1,4 +1,5 @@
 use pars_libs::{parse_line, Remote, RemoteCommand};
+use std::collections::VecDeque;
 use std::io::{self, BufRead};
 use std::process::{Command, ExitStatus};
 use std::sync::{Arc, Mutex};
@@ -18,7 +19,7 @@ struct ParallelCommand {
 }
 
 struct ParallelExecutor {
-    commands: Vec<ParallelCommand>,
+    commands: Vec<VecDeque<ParallelCommand>>,
     thread_limit: usize,
     counter: Arc<Mutex<usize>>,
     mode: String,
@@ -34,56 +35,40 @@ impl ParallelExecutor {
         }
     }
 
-    fn add_command(&mut self, command: String, args: Vec<String>) {
-        self.commands.push(ParallelCommand {
-            command,
-            args,
-            executed: false,
-            exit_status: None,
-        });
-    }
+    // fn add_command(&mut self, command: String, args: Vec<String>) {
+    //     self.commands.push_back(ParallelCommand {
+    //         command,
+    //         args,
+    //         executed: false,
+    //         exit_status: None,
+    //     });
+    // }
 
-    fn append_command(&mut self) {
-        while *self.counter.lock().unwrap() < self.thread_limit {
-            if let Some(command) = self.commands.pop() {
-                *self.counter.lock().unwrap() += 1;
-                std::thread::scope(|s| {
-                    let thread = s.spawn(move || {
-                        let mut cmd = Command::new(&command.command);
-                        cmd.args(&command.args);
-                        let code = cmd.status().unwrap().code();
-                        return code;
-                    });
-                    let res = thread.join().unwrap();
-                    *self.counter.lock().unwrap() -= 1;
-                    if res == Some(0) {
-                        todo!()
-                    }
-                });
-            }
-        }
-    }
 
     fn execute_commands(&mut self) {
-        for _ in 0..self.thread_limit {
-            if let Some(command) = self.commands.pop() {
-                *self.counter.lock().unwrap() += 1;
-                std::thread::scope(|s| {
-                    let thread = s.spawn(move || {
-                        let mut cmd = Command::new(&command.command);
-                        cmd.args(&command.args);
-                        let code = cmd.status().unwrap().code();
-                        return code;
-                    });
-                    let res = thread.join().unwrap();
-                    *self.counter.lock().unwrap() -= 1;
-                    if res == Some(0) {
-                        todo!()
-                    }
-                });
-            }
+        if *self.counter.lock().unwrap() >= self.thread_limit {
+            return;
+        }
+        // if let Some(command) = self.commands.first() {
+            *self.counter.lock().unwrap() += 1;
+          if  let Some(command) = self.commands.pop(){
+            let thread = std::thread::spawn(move || {
+              command.iter().for_each(|cmd|{
+                let mut com = Command::new(cmd.command.as_str());
+                    com.args(cmd.args.clone());
+                    let code = com.status();
+              });
+            });
+            // vec_threads.push(thread);
+            thread.join();
+            *self.counter.lock().unwrap() -= 1;
+            self.execute_commands();
         }
     }
+
+    // for thread in vec_threads {
+    //     thread.join().unwrap();
+    // }
 }
 
 fn main() {
@@ -113,20 +98,37 @@ fn main() {
     let stdin = std::io::stdin();
     let lines = stdin.lock().lines();
 
+    let mut exec = ParallelExecutor::new();
+
     for line in lines {
         let commands: Vec<Vec<String>> = parse_line(&line.unwrap()).unwrap();
-
+        let mut  cmds: VecDeque<ParallelCommand> = VecDeque::new();
         for command_args in commands {
-            let command = &command_args[0];
-            let args = &command_args[1..];
-            let status = Command::new(command).args(args).status();
-
-            if let Err(exit_status) = status {
-                println!("Command failed with exit status: {:?}", exit_status);
-                break; // Stop executing commands on this line
-            }
+            let para = ParallelCommand {
+                command: command_args[0].clone(),
+                args: command_args[1..].to_vec(),
+                executed: false,
+                exit_status: None,
+            };
+            cmds.push_back(para);
+            // println!("{:?}", para.command);
+            // println!("{:?}", para.args);
+            // exec.commands.push(para);
         }
+        exec.commands.push(cmds);
+        // for command_args in commands {
+        //     let command = &command_args[0];
+        //     let args = &command_args[1..];
+        //     let status = Command::new(command).args(args).status();
+
+        //     if let Err(exit_status) = status {
+        //         println!("Command failed with exit status: {:?}", exit_status);
+        //         break; // Stop executing commands on this line
+        //     }
+        // }
+        exec.execute_commands();
     }
+
     // println!("Number of -j: {}", num_j);
     // println!("Value of -r: {}", r_value);
 
